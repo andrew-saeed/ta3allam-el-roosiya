fa7c29c04bab34f7c4b07d0fe7db59c0d9bf3004
andrew-saeed <andrew-saeed.dev@outlook.com>
2024-12-24 00:00:00 +0000

    check user's answer and give feedback

--- src/views/alphabet/quiz.vue
+++ src/views/alphabet/quiz.vue
@@ -5,24 +5,29 @@ import PageLayout from '@/layouts/PageLayout.vue'
 
 import IconBtn from '@/components/IconBtn.vue'
 import Speaker from '@/components/icons/Speaker.vue'
+import X from '@/components/icons/X.vue'
+import Check from '@/components/icons/Check.vue'
 
 import useAlphabetStore from '@/stores/alphabet'
 
-const {data} = useAlphabetStore()
+const { data } = useAlphabetStore()
 const quiz = ref([])
+
+const shuffleData = arr => {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr
+}
 
 onMounted(() => {
 
-    const shuffledData = [...new Set(data)].sort(() => 0.5 - Math.random())
+    const shuffledData = shuffleData([...data])
 
     quiz.value = shuffledData.slice(0, 10).map((item) => {
 
-        const randomAnswers = Array.from({length: data.length}, (_, i) => i)
-        .sort(() => 0.5 - Math.random())
-        .slice(0, 4)
-        .map((index) => {
-            return data[index]
-        })
+        const randomAnswers = shuffleData(shuffledData).slice(0, 4)
 
         const duplicateAnswer = randomAnswers.find(answer => answer.id === item.id)
         if(!duplicateAnswer) {
@@ -31,6 +36,7 @@ onMounted(() => {
         }
 
         return {
+            answered: false,
             correctAnswer: item.id,
             sound: item.sound,
             answers: randomAnswers
@@ -52,21 +58,32 @@ const playSound = (file) => {
 <template>
     <PageLayout title="alphabet quiz" class="grid grid-rows-[min-content_1fr]">
         <div class="h-full flex justify-center items-center">
-            <ul class="space-y-24">
-                <li v-for="question in quiz" class="flex gap-4">
-                    <p>
+            <ul class="w-full space-y-24">
+                <li 
+                    v-for="question in quiz" 
+                    class="relative grid grid-cols-[min-content_min-content] justify-center gap-4 py-4" 
+                    :key="question.id"
+                >
+                    <p class="relative z-20">
                         <IconBtn class="big" @click="playSound(question.sound)">
                             <Speaker />
                         </IconBtn>
                     </p>
                     <ul class="flex flex-col gap-4 pt-8">
-                        <li 
-                            class="relative text-large p-8 bg-primary-600 border-4 border-primary-600 hover:border-surface-50 rounded-full cursor-pointer transition" 
-                            v-for="answer in question.answers" 
-                            @click="console.log(answer.id === question.correctAnswer)"
+                        <li
+                            v-for="answer in question.answers"
+                            :class="[{'z-20': answer.id == question.correctAnswer}, 'relative bg-primary-600 text-large p-8 border-4 border-primary-600 hover:border-surface-50 rounded-full cursor-pointer transition']" 
+                            @click="question.answered = true"
                         >
                             <span class="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]">{{ answer.c }}</span>
+                            <span v-show="question.answered && answer.id === question.correctAnswer" class="absolute top-0 left-full w-[2rem] h-[2rem]">
+                                <Check />
+                            </span>
+                            <span v-show="question.answered && answer.id !== question.correctAnswer" class="absolute top-0 left-full w-[1.5rem] h-[1.5rem]">
+                                <X />
+                            </span>
                         </li>
+                        <li :class="[{'!visible !opacity-60': question.answered}, 'invisible absolute top-0 left-0 z-10 w-full h-full bg-black rounded-lg opacity-0 transition duration-300']"></li>
                     </ul>
                 </li>
             </ul>
